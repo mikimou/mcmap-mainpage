5a2757446c9273a2891659074aa4335425d5d7b7
Michal Hicz <michal07.h@gmail.com>
2023-11-09 21:51:23 +0100

    added profiler

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -7,6 +7,7 @@
 		<a style="text-decoration: none; width:5em; height:2em; margin:1em; color:black; " href="https://uptime.michalhicz.eu/status/mc" class="drac-btn drac-bg-cyan-green drac-btn-sm">UPTIME</a>
 	</div>
 	<div class="corner">
+		<a style="float: right; text-decoration: none; width:5em; height:2em; margin:1em; color:black; " href="https://spark.lucko.me/elt6I05yMF" class="drac-btn drac-bg-cyan-green drac-btn-sm">STATS</a>
 	</div>
 </header>
 
